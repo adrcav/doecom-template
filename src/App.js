4746ffaf467e82114a601b2ddaf7ee714ba35980
Adriel Cavalcanti <cavalcantiadriel@gmail.com>
2020-06-20 10:42:59 -0300

    initial base project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/logo.png" className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
